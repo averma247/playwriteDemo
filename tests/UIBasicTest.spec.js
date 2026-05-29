407db968108c5dba9f3e85df9059984247412f57
Ajay Verma <ajay.verma@nagarro.com>
2026-05-30 03:58:17 +0530

    Added: Child Window handling code.

--- tests/UIBasicTest.spec.js
+++ tests/UIBasicTest.spec.js
@@ -10,17 +10,7 @@ test('testUIBasic', async ({ page }) => {
 
 });
 
-test('Google Page', async ({ page }) => {
-    //chrome - plugin/cookies
-    await page.goto('https://www.google.com/');
-    await expect(page).toHaveTitle('Google');
-    //await page.pause();
-    await page.getByLabel
-
-
-});
-
-test.only('Test Incorrect Credentials', async ({ page }) => {
+test('Test Incorrect Credentials', async ({ page }) => {
     const usernameTxtBox = page.locator('#username');
     const passwordTxtBox = page.locator('#password');
     const signInBtn = page.locator('#signInBtn');
@@ -32,6 +22,10 @@ test.only('Test Incorrect Credentials', async ({ page }) => {
     //css selector, Xpath
     await usernameTxtBox.fill('ajayverma');
     await passwordTxtBox.fill('abcd@123');
+
+    console.log(await usernameTxtBox.inputValue());
+    console.log(await passwordTxtBox.inputValue());
+
     await signInBtn.click();
     // await expect(page.locator('a').filter({ hasText: 'ProtoCommerce' }).first()).toBeVisible();
     console.log(await page.locator("[style*='block']").textContent());
@@ -45,12 +39,65 @@ test.only('Test Incorrect Credentials', async ({ page }) => {
     await signInBtn.click();
     // await page.waitForTimeout(5000)
 
+
     console.log(await cardTitles.first().textContent());
     console.log(await cardTitles.nth(1).textContent());
     console.log(await cardTitles.last().textContent());
     const allTitles = await cardTitles.allTextContents();
     console.log(allTitles);
 
+
+});
+
+test('UI Controls on Page', async ({ page }) => {
+    const usernameTxtBox = page.locator('#username');
+    const passwordTxtBox = page.locator('#password');
+    const signInBtn = page.locator('#signInBtn');
+    const dropdown = page.locator('select.form-control');
+
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+
+    await dropdown.selectOption('consult');
+
+    await page.locator('.radiotextsty').nth(1).click();
+    await page.locator('#okayBtn').click();
+    console.log(await page.locator('.radiotextsty').last().isChecked());
+    await expect(page.locator('.radiotextsty').last()).toBeChecked();
+    await page.locator('#terms').click();
+    await expect(page.locator('#terms')).toBeChecked();
+    await page.locator('#terms').uncheck();
+    //await expect(await page.locator('#terms').isChecked()).toBeFalsy();
+    await expect(page.locator('#terms')).not.toBeChecked();
+    //await page.waitForTimeout(3000);
+    await expect(page.locator('[href*=documents-request]')).toHaveAttribute('class', 'blinkingText');
+
+
+});
+
+test.only('Child Window handling', async ({ browser }) => {
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    const usernameTxtBox = page.locator('#username');
+
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    const documentLink = page.locator('[href*=documents-request]');
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'),
+        documentLink.click()
+    ]);
+    await newPage.waitForLoadState();
+    const childWindowText = await newPage.locator('.red').textContent();
+    console.log(childWindowText);
+    const arrchildWindowText = childWindowText.split('@');
+    const domain = arrchildWindowText[1].split(' ')[0];
+    console.log(domain);
+
+    await usernameTxtBox.fill(domain);
+    // await page.pause();
+
+    console.log('Username Entered:', await usernameTxtBox.inputValue());
 
 
 
