fcbb889b30dcbf08bc0453f0a80e096737449f20
Ajay Verma <ajay.verma@nagarro.com>
2026-06-02 00:54:46 +0530

    added more code for client app

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -15,6 +15,10 @@ test('Client App Test', async ({ page }) => {
     const usernameTxtBox = page.locator('#userEmail');
     const passwordTxtBox = page.locator('#userPassword');
     const signInBtn = page.locator('#login');
+    let desiredProduct = 'ZARA COAT 3';
+
+    const products = page.locator('.card-body');
+
 
     await page.goto('https://rahulshettyacademy.com/client');
     console.log(await page.title());
@@ -26,11 +30,29 @@ test('Client App Test', async ({ page }) => {
     console.log(await passwordTxtBox.inputValue());
 
     await signInBtn.click();
-    await page.waitForLoadState('networkidle');// Wait for the page to load after clicking the sign-in button
-
+    //await page.waitForLoadState('networkidle');// Wait for the page to load after clicking the sign-in button
+    await page.locator('.card-body b').first().waitFor(); // Wait for the first card title to be visible
     const cardTitles = await page.locator('.card-body b').allTextContents();
     console.log(cardTitles);
-    
+
+    const productCount = await products.count();
+    console.log('Product Count: ' + productCount);
+
+    for (let i = 0; i < productCount; i++) {
+        const productName = await products.nth(i).locator('b').textContent();
+        if (productName === desiredProduct) {
+            await products.nth(i).locator('text= Add To Cart').click();
+            break;
+        }
+    }
+
+    await page.locator("[routerlink*='cart']").click();
+    //await page.locator('h3:has-text("ZARA COAT 3")').isVisible();
+    expect(page.locator('h3:has-text("' + desiredProduct + '")')).toBeVisible();
+
+    await page.pause();
+
+
 
 
 
