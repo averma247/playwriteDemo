4036c658098a3b0397a4e2b89f61715c10ca5c01
Ajay Verma <ajay.verma@nagarro.com>
2026-07-01 00:36:49 +0530

    fix: sync issue in calendar

--- tests/Calender.spec.js
+++ tests/Calender.spec.js
@@ -1,8 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('Calendar Test', async ({ page }) => {
-    const monthNumber = "12";
-    const yearNumber = "2026";
+    const monthNumber = "6";
+    const yearNumber = "2027";
     const dateNumber = "7";
     const expectedDateList = [monthNumber, dateNumber, yearNumber];
 
@@ -16,13 +16,14 @@ test('Calendar Test', async ({ page }) => {
     await page.locator('.react-calendar__year-view__months__month').nth(Number(monthNumber) - 1).click();
     await page.locator(`//abbr[text()='${dateNumber}']`).click();
 
+    //await page.waitForTimeout(2000); // Only for debugging
     const inputs = await page.locator('.react-date-picker__inputGroup__input');
 
     for (let i = 0; i < expectedDateList.length; i++) {
 
         const inputValue = await inputs.nth(i).inputValue();
         console.log(inputValue);
-        expect(inputValue).toEqual(expectedDateList[i]);
+        await expect(inputs.nth(i)).toHaveValue(expectedDateList[i]);
     }
 
 
